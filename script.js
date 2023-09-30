1556c060201687907fd3de5f2491206287f97193
manuel vimercati <manuel.vimercati92@gmail.com>
2023-09-30 17:48:48 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,103 @@
+function expandFCuor() {
+    let Inizio = document.getElementById("Inizio");
+    let InizioTesto = document.getElementById("InizioTesto");
+    Inizio.classList.toggle("expanded");
+    InizioTesto.classList.toggle("hidden");
+    InizioTesto.classList.toggle("visibleInizio");
+}
+
+function expandFaseEroi() {
+    let faseEroi = document.getElementById("faseEroi");
+    let faseEroiTesto = document.getElementById("faseEroiTesto");
+    faseEroi.classList.toggle("expanded");
+    faseEroiTesto.classList.toggle("hidden");
+    faseEroiTesto.classList.toggle("visible");
+}
+
+function expandFaseMov() {
+    let faseMov = document.getElementById("faseMov");
+    let faseMovTesto = document.getElementById("faseMovTesto");
+    faseMov.classList.toggle("expanded");
+    faseMovTesto.classList.toggle("hidden");
+    faseMovTesto.classList.toggle("visible");
+}
+
+function expandFaseCar() {
+    let faseCar = document.getElementById("faseCar");
+    let faseCarTesto = document.getElementById("faseCarTesto");
+    faseCar.classList.toggle("expanded");
+    faseCarTesto.classList.toggle("hidden");
+    faseCarTesto.classList.toggle("visible");
+}
+
+function expandFaseCombat() {
+    let faseCombat = document.getElementById("faseCombat");
+    let faseCombatTesto = document.getElementById("faseCombatTesto");
+    faseCombat.classList.toggle("expanded");
+    faseCombatTesto.classList.toggle("hidden");
+    faseCombatTesto.classList.toggle("visible");
+}
+
+function expandFaseShock() {
+    let faseShock = document.getElementById("faseShock");
+    let faseShockTesto = document.getElementById("faseShockTesto");
+    faseShock.classList.toggle("expanded");
+    faseShockTesto.classList.toggle("hidden");
+    faseShockTesto.classList.toggle("visible");
+}
+
+function expandAlarielle() {
+    let Alarielle = document.getElementById("Alarielle");
+    let AlarielleTesto = document.getElementById("AlarielleTesto");
+    Alarielle.classList.toggle("expanded");
+    AlarielleTesto.classList.toggle("hidden");
+    AlarielleTesto.classList.toggle("visible");
+}
+
+function expandLady() {
+    let Lady = document.getElementById("Lady");
+    let LadyTesto = document.getElementById("LadyTesto");
+    Lady.classList.toggle("expanded");
+    LadyTesto.classList.toggle("hidden");
+    LadyTesto.classList.toggle("visible");
+}
+
+function expandDurthu() {
+    let Durthu = document.getElementById("Durthu");
+    let DurthuTesto = document.getElementById("DurthuTesto");
+    Durthu.classList.toggle("expanded");
+    DurthuTesto.classList.toggle("hidden");
+    DurthuTesto.classList.toggle("visible");
+}
+
+function expandArcieri() {
+    let Arcieri = document.getElementById("Arcieri");
+    let ArcieriTesto = document.getElementById("ArcieriTesto");
+    Arcieri.classList.toggle("expanded");
+    ArcieriTesto.classList.toggle("hidden");
+    ArcieriTesto.classList.toggle("visible");
+}
+
+function expandDriadi() {
+    let Driadi = document.getElementById("Driadi");
+    let DriadiTesto = document.getElementById("DriadiTesto");
+    Driadi.classList.toggle("expanded");
+    DriadiTesto.classList.toggle("hidden");
+    DriadiTesto.classList.toggle("visible");
+}
+
+function expandTreeRevenant() {
+    let TreeRevenant = document.getElementById("TreeRevenant");
+    let TreeRevenantTesto = document.getElementById("TreeRevenantTesto");
+    TreeRevenant.classList.toggle("expanded");
+    TreeRevenantTesto.classList.toggle("hidden");
+    TreeRevenantTesto.classList.toggle("visible");
+}
+
+function expandBosco() {
+    let Bosco = document.getElementById("Bosco");
+    let BoscoTesto = document.getElementById("BoscoTesto");
+    Bosco.classList.toggle("expanded");
+    BoscoTesto.classList.toggle("hidden");
+    BoscoTesto.classList.toggle("visible");
+}
